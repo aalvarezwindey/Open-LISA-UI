6ea276e82a95d7738b6eb47e49736a3aa2a61990
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2022-04-17 13:36:57 -0300

    update images

---
+++ frontend/src/domain/constants.js
@@ -0,0 +1,30 @@
+const DEFAULT_IMAGE_BASE_PATH = '/images/default/';
+export const DEFAULT_IMAGES = {
+  NONE: {
+    path: DEFAULT_IMAGE_BASE_PATH + 'none.png',
+  },
+
+  CAMERA: {
+    path: DEFAULT_IMAGE_BASE_PATH + 'camera.png',
+  },
+
+  DAQ: {
+    path: DEFAULT_IMAGE_BASE_PATH + 'daq.png',
+  },
+
+  FUNCTIONS_GENERATOR: {
+    path: DEFAULT_IMAGE_BASE_PATH + 'functions_generator.png',
+  },
+
+  MULTIMETER: {
+    path: DEFAULT_IMAGE_BASE_PATH + 'multimeter.png',
+  },
+
+  OSCILLOSCOPE: {
+    path: DEFAULT_IMAGE_BASE_PATH + 'oscilloscope.png',
+  },
+
+  POWER_SUPPLY: {
+    path: DEFAULT_IMAGE_BASE_PATH + 'power_supply.png',
+  },
+};
