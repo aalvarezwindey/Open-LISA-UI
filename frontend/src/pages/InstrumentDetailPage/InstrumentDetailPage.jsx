27883b9e30541cc7c6875c230905b8d4945218e9
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2022-04-29 19:32:09 -0300

    display edit instrument form

--- frontend/src/pages/InstrumentDetailPage/InstrumentDetailPage.jsx
+++ frontend/src/pages/InstrumentDetailPage/InstrumentDetailPage.jsx
@@ -1,15 +1,68 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useDisclosure } from '@chakra-ui/react';
+import BasicModal from '../../components/BasicModal/BasicModal';
 import PageBody from '../../components/Layout/PageBody/PageBody';
+import InstrumentForm, {
+  InstrumentFormFileds,
+} from '../../domain/components/InstrumentForm/InstrumentForm';
+import { INSTRUMENT_FIELD_NAMES } from '../../domain/constants';
+import useForm from '../../hooks/useForm';
 import useInstrumentDetail from '../../hooks/useInstrumentDetail';
 import InstrumentDetail from './components/InstrumentDetail';
 
 export default function InstrumentDetailPage() {
   const { instrumentId } = useParams();
+  const {
+    isOpen: editModalIsOpen,
+    onOpen: openEditModal,
+    onClose: closeEditModal,
+  } = useDisclosure();
   const { data: instrument } = useInstrumentDetail(instrumentId);
+  const { isValid, reset, displayErrors, updateField, ...formProps } = useForm({
+    fields: InstrumentFormFileds,
+  });
+
+  const setFormWithCurrentInstrumentValues = useCallback(() => {
+    if (instrument) {
+      updateField(INSTRUMENT_FIELD_NAMES.BRAND)(instrument.brand);
+      updateField(INSTRUMENT_FIELD_NAMES.MODEL)(instrument.model);
+      updateField(INSTRUMENT_FIELD_NAMES.PHYSICAL_ADDRESS)(instrument.physicalAddress);
+      updateField(INSTRUMENT_FIELD_NAMES.DESCRIPTION)(instrument.description);
+
+      const imageUrlParts = instrument.image.split('/');
+      const imageFileName = imageUrlParts[imageUrlParts.length - 1];
+      updateField(INSTRUMENT_FIELD_NAMES.IMAGE)(imageFileName);
+    }
+  }, [updateField, instrument]);
+
+  useEffect(() => {
+    setFormWithCurrentInstrumentValues();
+  }, [setFormWithCurrentInstrumentValues]);
+
+  const handleEditInstrument = () => {};
+
+  const handleCloseEditModal = () => {
+    setFormWithCurrentInstrumentValues();
+    closeEditModal();
+  };
+
   return (
     <PageBody>
-      <InstrumentDetail {...instrument} />
+      <InstrumentDetail {...instrument} onEditInstrument={openEditModal} />
+      <BasicModal
+        title="Editar instrumento"
+        onClose={handleCloseEditModal}
+        isOpen={editModalIsOpen}
+        primaryAction={{
+          label: 'Confirmar',
+          onAction: handleEditInstrument,
+          loading: false, // TODO
+        }}
+        secondaryAction={{ label: 'Cancelar', onAction: handleCloseEditModal }}
+      >
+        <InstrumentForm {...formProps} updateField={updateField} />
+      </BasicModal>
     </PageBody>
   );
 }
--- frontend/src/pages/InstrumentDetailPage/components/InstrumentDetail.jsx
+++ frontend/src/pages/InstrumentDetailPage/components/InstrumentDetail.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Code, Flex, Heading, Image, Text } from '@chakra-ui/react';
 
-function InstrumentDetail({ brand, model, image, physicalAddress, description }) {
+function InstrumentDetail({ brand, model, image, physicalAddress, description, onEditInstrument }) {
   return (
     <Flex h="50vh" direction="row">
       <Image
@@ -26,7 +26,9 @@ function InstrumentDetail({ brand, model, image, physicalAddress, description })
           {description ? <Text>{description}</Text> : null}
         </Flex>
         <Flex>
-          <Button colorScheme="blue">Editar instrumento</Button>
+          <Button colorScheme="blue" onClick={onEditInstrument}>
+            Editar instrumento
+          </Button>
           <Button ml={4} colorScheme="red">
             Eliminar instrumento
           </Button>
@@ -42,6 +44,7 @@ InstrumentDetail.propTypes = {
   image: PropTypes.string.isRequired,
   physicalAddress: PropTypes.string,
   description: PropTypes.string,
+  onEditInstrument: PropTypes.func.isRequired,
 };
 
 export default InstrumentDetail;
