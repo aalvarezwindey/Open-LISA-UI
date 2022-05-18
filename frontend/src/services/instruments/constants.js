83e5161287b7ca8eb3eb641142126b62dcd323a2
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2022-05-18 16:02:10 -0300

    add getInstrumentCommands service

--- frontend/src/services/instruments/constants.js
+++ frontend/src/services/instruments/constants.js
@@ -3,4 +3,5 @@ export const URI = {
   INSTRUMENTS_IMAGES: '/instruments/images',
   DETECTED_PHYSICAL_ADDRESSES: '/physical-addresses/detected',
   INSTRUMENT_BY_ID: '/instruments/:instrumentId',
+  INSTRUMENT_COMMANDS: '/instruments/:instrumentId/commands',
 };
