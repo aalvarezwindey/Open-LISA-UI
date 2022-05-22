249f7514ac2fe09bf48fbc6e0086c7a65d24e439
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2022-05-22 18:41:34 -0300

    update SCPI commands constants

--- frontend/src/domain/constants.js
+++ frontend/src/domain/constants.js
@@ -8,3 +8,44 @@ export const INSTRUMENT_FIELD_NAMES = {
   DESCRIPTION: 'description',
   IMAGE: 'image',
 };
+
+export const SCPI_COMMAND_FIELD_NAMES = {
+  NAME: 'name',
+  COMMAND: 'command',
+  TYPE: 'type',
+  DESCRIPTION: 'description',
+  PARAMS: 'params',
+};
+
+export const SCPI_COMMAND_TYPES = [
+  {
+    label: 'Set',
+    value: 'set',
+  },
+  {
+    label: 'Query',
+    value: 'query',
+  },
+  {
+    label: 'Query Buffer',
+    value: 'query_buffer',
+  },
+];
+
+export const SCPI_COMMAND_PARAM_VALUE_TYPES = [
+  {
+    label: 'int',
+    value: 'int',
+    example: 1,
+  },
+  {
+    label: 'float',
+    value: 'float',
+    example: 1.5,
+  },
+  {
+    label: 'string',
+    value: 'string',
+    example: 'A',
+  },
+];
