c92acd466fee34f51dacd6e66052a7648c00ff1a
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2022-07-13 21:25:14 -0300

    Add entrypoint in navbar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { Box, Heading } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import { Box, Button, Heading } from '@chakra-ui/react';
+import { ReactComponent as ConnectionIcon } from '../../assets/connection.svg';
+import { ROUTES } from '../../routing/routes';
 
 export default function NavBar() {
+  const navigate = useNavigate();
   return (
     <Box
       as="nav"
@@ -9,14 +13,21 @@ export default function NavBar() {
       bgColor="navbar"
       display="flex"
       alignItems="center"
+      justifyContent="space-between"
       shadow="xl"
       pos="sticky"
       zIndex={100}
       top={0}
+      p={4}
+      w="100%"
     >
-      <Heading ml={4} color="text.primary">
-        Open LISA
-      </Heading>
+      <Heading color="text.primary">Open LISA</Heading>
+      <Button variant="ghost" onClick={() => navigate(ROUTES.SETTINGS)}>
+        <Box mr={2}>
+          <ConnectionIcon width={25} height={25} />
+        </Box>
+        Conexión con el servidor
+      </Button>
     </Box>
   );
 }
